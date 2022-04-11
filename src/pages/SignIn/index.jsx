eb7018f1c0730b056645f8f5226123e84b9dad22
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-04-11 14:41:22 -0300

    FEAT - Layout da tela SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,14 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import {Input} from "../../components/Input";
 
 import {
     Container,
+    Logo,
+    Form
 } from "./styles";
 
 
 export function SignIn() {
+    const [disable, setDisable] = React.useState(false);
+    const [userMail, setUserMail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    
     return (
         <Container>
-            <h1>SignIn</h1>
+            <Logo>
+                <img src="/logo.png" alt="Logo da TrackIt" ></img>
+                <h1>TrackIt</h1>
+            </Logo>
+            <Form>
+                <input 
+                    value={userMail} 
+                    onChange={(e)=>{setUserMail(e.target.value)}} 
+                    type="email" 
+                    placeholder="email" 
+                    required 
+                    disabled={disable}
+                />
+                <input 
+                    value={password} 
+                    onChange={(e)=>{setPassword(e.target.value)}} 
+                    type="text" 
+                    placeholder="senha" 
+                    required 
+                    disabled={disable}
+                />
+                <button>Entrar</button>
+                <Link to="/register">Não tem uma conta? Cadastre-se!</Link>
+            </Form>
         </Container>
     )
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -4,7 +4,71 @@ import styled from "styled-components"
 export const Container = styled.section`
     display: flex;
     justify-content: center;
+    flex-direction: column;
     height: 100vh;
     background-color: #76fa9e;
     background-image: linear-gradient(169deg, #76fa9e 0%, #00b0d9 55%);
+`;
+
+export const Logo = styled.div`
+    text-align: center;
+    margin-top: 2rem;
+
+    img {
+        width: 10rem;
+    }
+
+    h1 {
+        font-size: 3rem;
+        color: var(--white);
+    }
+`;
+
+export const Form = styled.form`
+    flex: 1;
+    display: flex;
+    text-align: center;
+    gap: 0.25rem;
+    width: 100%;
+    margin: 3rem 0;
+    padding: 0 2rem;
+    flex-direction: column;
+
+    input {
+        padding: 0.75rem 1rem;
+        font-size: 1.25rem;
+        border: 2px solid var(--blue);
+        border-radius: 4px;
+        transition: 200ms;
+        color: var(--black);
+        :focus {
+            outline:solid 2px var(--green);
+        }
+    }
+
+    button {
+        background-color: white;
+        color: var(--secondary);
+        max-width: 16rem;
+        border-radius: 2rem;
+        border: 2px solid var(--secondary-ligth);
+        padding: 0.75rem 2rem;
+        font-size: 1.25rem;
+        margin: 1rem auto 0.75rem;
+        -webkit-box-shadow: 2px 2px 21px -10px rgba(25,25,25,1);
+        -moz-box-shadow: 2px 2px 21px -10px rgba(25,25,25,1);
+        box-shadow: 2px 2px 21px -10px rgba(25,25,25,1);
+        transition: 200ms;
+
+        :hover {
+            background-color: #73F9B870;
+            color: white;
+        }
+    }
+
+    a {
+        color: var(--green);
+        font-weight: 700;
+        font-size: 1rem;
+    }
 `;
